337586f2a8ad8f4cebe26375717aff6e5d19f262
Andy Weiss <andyweiss1982@gmail.com>
2018-11-14 12:47:39 -0500

    first commit

---
+++ app/models/thing_with_react_form.rb
@@ -0,0 +1,2 @@
+class ThingWithReactForm < ApplicationRecord
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :thing_with_react_forms
+  resources :thing_with_rails_forms
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
